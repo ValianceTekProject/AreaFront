53b7342d13feb4b5e5b50ebfca21797a14d221b9
Yuuko <tilio.huart@epitech.eu>
2025-11-26 13:33:29 +0100

    fix: update api root

--- poc_front/app/api/todo/route.ts
+++ poc_front/app/api/todo/route.ts
@@ -1,16 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const BACKEND_URL = "http://localhost:8080/todos";
+const BACKEND_URL = process.env.API_URL || 'http://back:8080';
 
 export async function GET() {
-  const res = await fetch(`${BACKEND_URL}/`);
+  const res = await fetch(`${BACKEND_URL}/todos`);
   const data = await res.json();
   return NextResponse.json(data);
 }
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  const res = await fetch(`${BACKEND_URL}/add`, {
+  const res = await fetch(`${BACKEND_URL}/todos/add`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
@@ -20,7 +20,7 @@ export async function POST(req: NextRequest) {
 
 export async function DELETE(req: NextRequest) {
   const body = await req.json();
-  const res = await fetch(`${BACKEND_URL}/del`, {
+  const res = await fetch(`${BACKEND_URL}/todos/del`, {
     method: "DELETE",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
@@ -30,7 +30,7 @@ export async function DELETE(req: NextRequest) {
 
 export async function PATCH(req: NextRequest) {
   const body = await req.json();
-  const res = await fetch(`${BACKEND_URL}/mod`, {
+  const res = await fetch(`${BACKEND_URL}/todos/mod`, {
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
